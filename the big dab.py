0eacf99a9478b36c5bfeba29682edba7c8d3ff1e
=James Sellman <=james.b.sellman@googlemail.com>
2019-09-15 11:38:23 +0100

    editting the big dab

--- the big dab.py
+++ the big dab.py
@@ -2,4 +2,4 @@ People = input("Do you love your wife?")
 if People = "yes":
     print("That's my jam, that's my jam, that's my jam, that's my jam")
 else:
-    print("What a hot shower of shit")
+    print("What a hot shower of shit") #hot shower? no shit
